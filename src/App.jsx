d47807c19a7d354feac10a599e4151460c475c75
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2024-04-29 21:38:39 +0100

    Add Title component above Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Programs from "./components/Programs/Programs";
+import Title from "./components/Title/Title";
 
 const App = () => {
-
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Title subTitle="Our Programmes" title="What We Offer" />
+        <Programs />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
