eddfdfe5ea997e3b93179e752fe5785467073697
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2024-05-08 18:00:30 +0100

    Complete About component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.png'
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
+  const [sticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setSticky(true) : setSticky(false);
+    });
+  }, []);
+
   return (
-    <nav className="navbar-container">
-      <img src={logo} alt="Education logo with a graduate hat" className="logo"
-       />
+    <nav className={`navbar-container ${sticky ? "dark-nav" : ""}`}>
+      <img
+        src={logo}
+        alt="Education logo with a graduate hat"
+        className="logo"
+      />
       <ul>
         <li>Home</li>
         <li>Programme</li>
@@ -14,10 +25,12 @@ const Navbar = () => {
         <li>Campus</li>
         <li>Testimonials</li>
         <li>Contact</li>
-        <li><button className="btn">Contact Us</button></li>
+        <li>
+          <button className="btn">Contact Us</button>
+        </li>
       </ul>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
